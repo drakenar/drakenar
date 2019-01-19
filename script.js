c1f0b373dbcca7838b819ec23c24f1fb16cfc691
Santiago Posada <santiago@sposada.sposada.co>
2019-01-19 04:49:15 -0500

    Ajax

--- script.js
+++ script.js
@@ -12,6 +12,42 @@ document.addEventListener('DOMContentLoaded',() => {
         /*Array.from(inputs).forEach((val, key) => {
             console.log(vals);
         });*/
-        alert('lol');
+        /*xhttp.open("POST", "http://bdb9146b.ngrok.io/pfsservice", true);
+        xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xhttp.setRequestHeader( 'referrer', 'cardseed');
+        xhttp.onreadystatechange = () => {
+            if (this.readyState == 4 && this.status == 200)
+                console.log(this.responseText);
+        }
+
+        xhttp.send("pfsform=abc");*/
+
+        let xhr = new XMLHttpRequest();
+        xhr.withCredentials = true;
+
+        xhr.addEventListener("readystatechange", () => {
+            if (this.readyState === 4) {
+            console.log(this.responseText);
+            }
+        });
+
+        xhr.open("GET", "http://bdb9146b.ngrok.io/pfsservice?test=abc");
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhr.setRequestHeader("cache-control", "no-cache");
+        xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+
+        xhr.send();
     });
+
+    /*send.addEventListener('click', () => {
+        console.log(form.elements);
+
+        Array.from(form.elements).forEach((a, b) => {
+            console.log(b.value);
+        });
+        /*for (let input in inputs) {
+            console.log(input);
+        }
+    });*/
+
 });
