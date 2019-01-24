49ac85d6f2c451ebe63e6d361b3045545c4705a7
drakenar <46695726+drakenar@users.noreply.github.com>
2019-01-24 17:15:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ document.addEventListener('DOMContentLoaded',() => {
         }
 
         let json = JSON.stringify(pfsForm);
-        window.open("http://63828450.ngrok.io/pfsservice/?pfsform="+json, "_self");
+        window.open("http://b199bf30.ngrok.io/pfsservice/?pfsform="+json, "_self");
 
         /*let xhr = new XMLHttpRequest();
         xhr.withCredentials = true;
